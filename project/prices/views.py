2ea93ad2c580b7c36019af74277c206d80018b49
Kim hyeongjin <127001416+OneDayOneAlgorithm@users.noreply.github.com>
2023-03-16 22:13:49 +0900

    1

--- project/prices/views.py
+++ project/prices/views.py
@@ -2,20 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 def price(request, thing, cnt):
-    
-    context = {
-        "라면":["라면",cnt,980*cnt],
-        "홈":["홈",cnt,1500*cnt],
-        "칙촉":["칙촉",cnt,2300*cnt],
-        "식빵":["식빵",cnt,1800*cnt]
-    }
+
     if thing == '라면':
-        return render(request,'price.html',context['라면'])
+        context = {
+            'thing':'라면',
+            'cnt' : cnt,
+            'total' : 980*cnt
+        }
     elif thing == '홈런볼':
-        return render(request,'price.html',context['홈런볼'])
+        context = {
+            'thing':'홈런볼',
+            'cnt' : cnt,
+            'total' : 1500*cnt
+        }
     elif thing == '칙촉':
-        return render(request,'price.html',context['칙촉'])
+        context = {
+            'thing':'칙촉',
+            'cnt' : cnt,
+            'total' : 2300*cnt
+        }
     elif thing == '식빵':
-        return render(request,'price.html',context['식빵'])
+        context = {
+            'thing':'식빵',
+            'cnt' : cnt,
+            'total' : 1800*cnt
+        }
     else:
-        return 
+        context = {
+            'thing' : thing
+        }
+    return render(request,'price.html',context)
